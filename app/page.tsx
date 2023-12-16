0d5a159976cee24623cb187aeb2e7302d6783dca
danmiles <41701228+danmiles@users.noreply.github.com>
2023-12-16 13:59:36 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from 'next/image'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+  const {userId} = auth()
+  // redirect to dashboard if user is logged in
+  if (userId) {
+    redirect('/dashboard');
+  }
   return (
     <main className="flex justify-center items-center h-screen p-2">      
       <h1 className="lg:text-5xl md:text-3xl text-2xl font-bold">Clerk + Next.js 14 Authentification</h1>
